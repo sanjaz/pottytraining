3a74ab4a2f11f55158fb5d84569a27c0916d8717
Sanja Segan <sanjazivotic@gmail.com>
2022-08-25 11:37:11 +0200

    Add custom router with customized creation url path.

--- pottytraining/users/urls.py
+++ pottytraining/users/urls.py
@@ -1,5 +1,5 @@
 from django.urls import include, path
-from rest_framework import routers
+from rest_framework.routers import DefaultRouter, DynamicRoute, Route
 
 from pottytraining.users.views import (
     AdminViewSet,
@@ -10,8 +10,58 @@ from pottytraining.users.views import (
 )
 
 
+class CustomRouter(DefaultRouter):
+    routes = [
+        # List route.
+        Route(
+            url=r'^{prefix}{trailing_slash}$',
+            mapping={'get': 'list',},
+            name='{basename}-list',
+            detail=False,
+            initkwargs={'suffix': 'List'}
+        ),
+        # Create route.
+        Route(
+            url=r'^{prefix}/create{trailing_slash}$',
+            mapping={'post': 'create'},
+            name='{basename}-create',
+            detail=False,
+            initkwargs={'suffix': 'Create'}
+        ),
+        # Dynamically generated list routes. Generated using
+        # @action(detail=False) decorator on methods of the viewset.
+        DynamicRoute(
+            url=r'^{prefix}/{url_path}{trailing_slash}$',
+            name='{basename}-{url_name}',
+            detail=False,
+            initkwargs={}
+        ),
+        # Detail route.
+        Route(
+            url=r'^{prefix}/{lookup}{trailing_slash}$',
+            mapping={
+                'get': 'retrieve',
+                'put': 'update',
+                'patch': 'partial_update',
+                'delete': 'destroy'
+            },
+            name='{basename}-detail',
+            detail=True,
+            initkwargs={'suffix': 'Instance'}
+        ),
+        # Dynamically generated detail routes. Generated using
+        # @action(detail=True) decorator on methods of the viewset.
+        DynamicRoute(
+            url=r'^{prefix}/{lookup}/{url_path}{trailing_slash}$',
+            name='{basename}-{url_name}',
+            detail=True,
+            initkwargs={}
+        ),
+    ]
+
+
 # Routers provide an easy way of automatically determining the URL conf.
-router = routers.DefaultRouter()
+router = CustomRouter()
 router.register(r'users', UserViewSet)
 router.register(r'admins', AdminViewSet, basename="admins")
 router.register(r'teachers', TeacherViewSet, basename="teachers")
@@ -19,6 +69,6 @@ router.register(r'parents', ParentViewSet, basename="parents")
 
 
 urlpatterns = [
-    path('users/create/', CreateUserView.as_view(), name="create_user"),
+    path('users/create/v1/', CreateUserView.as_view(), name="create_user"),
     path('', include(router.urls)),
 ]

5258815dd581bf6e8a9a17c613a9b2e943f2fa88
Sanja Segan <sanjazivotic@gmail.com>
2022-08-25 11:36:36 +0200

    Add pk in user creation serializer.

--- pottytraining/users/serializers.py
+++ pottytraining/users/serializers.py
@@ -21,7 +21,7 @@ class CreateUserSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = User
-        fields = ('username', 'email', 'password', 'first_name', 'last_name')
+        fields = ('username', 'email', 'password', 'first_name', 'last_name', 'pk')
         extra_kwargs = {'password': {'write_only': True}}
 
     def create(self, validated_data):

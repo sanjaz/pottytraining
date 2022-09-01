b6449a7702d5f7a40004d157ddcb738ddc248501
Sanja Segan <sanjazivotic@gmail.com>
2022-09-01 13:43:27 +0200

    Filter kids.

---
+++ pottytraining/kids/filters.py
@@ -0,0 +1,18 @@
+from django.contrib.auth import get_user_model
+from django_filters import rest_framework as filters
+
+from pottytraining.kids.models import Kid
+
+
+class KidFilter(filters.FilterSet):
+    first_name = filters.CharFilter(lookup_expr="iexact")
+    last_name = filters.CharFilter(lookup_expr="iexact")
+    teacher = filters.ModelChoiceFilter(
+        queryset=get_user_model().objects.filter(groups__name="Teachers"),
+        label="Teacher",
+        field_name="guardians",
+    )
+
+    class Meta:
+        model = Kid
+        fields = ["first_name", "last_name", "teacher"]
--- pottytraining/kids/views.py
+++ pottytraining/kids/views.py
@@ -1,10 +1,14 @@
 from pottytraining.kids.serializers import KidSerializer
+from django_filters import rest_framework as filters
 from rest_framework import viewsets
 
+from pottytraining.kids.filters import KidFilter
 from pottytraining.kids.models import Kid
 from pottytraining.kids.serializers import KidSerializer
 
 
 class KidViewSet(viewsets.ModelViewSet):
+    filter_backends = (filters.DjangoFilterBackend,)
+    filterset_class = KidFilter
     queryset = Kid.objects.all()
     serializer_class = KidSerializer
